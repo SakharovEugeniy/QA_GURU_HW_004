6a2997548350b41c88bc307c46745ebef1e290e5
Evgenii Sakharov <ESakharov@ibs.root.ibscorp.ru>
2024-11-28 14:03:22 +0300

    final version 1.002

--- src/test/java/GiithubSelendeTest.java
+++ src/test/java/GiithubSelendeTest.java
@@ -1,14 +1,29 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class GiithubSelendeTest {
+
+    String codeText =
+"""
+@ExtendWith({SoftAssertsExtension.class})
+class Tests {
+  @Test
+  void test() {
+    Configuration.assertionMode = SOFT;
+    open("page.html");
+
+    $("#first").should(visible).click();
+    $("#second").should(visible).click();
+  }
+}
+""";
 
     @BeforeAll
     static void BeforeAll(){
@@ -23,9 +38,7 @@ public class GiithubSelendeTest {
         $("#wiki-tab").click();
         $(".wiki-more-pages-link").$("button").click();
         $("[class=' js-wiki-sidebar-toggle-display']").find(byText("SoftAssertions")).click();
-        $$("div.markdown-heading").findBy(text("Using JUnit5 extend test class:")).sibling(0)
-                .shouldHave(text("SoftAssertsExtension"));   //на мой взгляд в данном задании приоритетным было посмотреть,
-                                                             //что именно после заголовка JUnit5 идёт пример с этим кодом
+        $$("div.markdown-heading").findBy(text("Using JUnit5 extend test class:")).sibling(0).shouldHave(exactText(codeText));
     }
 
 }
